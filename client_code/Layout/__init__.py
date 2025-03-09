99455d82a566764eb52bbd7d1e3dae11fb85e75c
harrygorden <harrygorden@gmail.com>
2025-03-09 12:08:24 -0500

    introduce navigation buttons

--- client_code/Layout/__init__.py
+++ client_code/Layout/__init__.py
@@ -7,6 +7,9 @@ import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 import anvil.server
 from datetime import datetime, timezone
+# Import the forms for the content slot
+from ..MarketSummary import MarketSummary
+from ..AllLines import AllLines
 
 
 class Layout(LayoutTemplate):
@@ -35,4 +38,19 @@ class Layout(LayoutTemplate):
     else:
         self.label_market_events.text = "This Week's Remaining Events"
 
+    # Set initial content for content_slot (optional)
+    self.content_panel.add_component(MarketSummary(), slot='content_slot')
+
     # Any code you write here will run before the form opens.
+    
+  def outlined_button_link_marketsummary_click(self, **event_args):
+    """This method is called when the Market Summary button is clicked"""
+    # Clear the current content in the slot and add the MarketSummary form
+    self.content_panel.clear_slot('content_slot')
+    self.content_panel.add_component(MarketSummary(), slot='content_slot')
+
+  def outlined_button_alllines_click(self, **event_args):
+    """This method is called when the All Lines button is clicked"""
+    # Clear the current content in the slot and add the AllLines form
+    self.content_panel.clear_slot('content_slot')
+    self.content_panel.add_component(AllLines(), slot='content_slot')
